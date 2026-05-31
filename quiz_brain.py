b802024a126de8d3770132c3adffd1defd11482f
Alice Dias <58340504+heyliceeee@users.noreply.github.com>
2026-05-31 15:05:22 +0100

    ✨feat: initialize project

---
+++ quiz_brain.py
@@ -0,0 +1,28 @@
+class QuizBrain:
+
+    def __init__(self, q_list):
+        self.question_number = 0
+        self.score = 0
+        self.question_list = q_list
+        self.current_question = None
+
+    def still_has_questions(self):
+        """Returns True or False depending on whether there are still questions left."""
+        return self.question_number < len(self.question_list)
+    def next_question(self):
+        """Advances the quiz to the next question."""
+        self.current_question = self.question_list[self.question_number] # Assigns the current question to the next question in the list
+        self.question_number += 1 # Increments the question number
+        user_answer = input(f"Q.{self.question_number}: {self.current_question.text} (True/False): ") # Asks the user the question and stores the answer
+        self.check_answer(user_answer) # Checks the user's answer
+    def check_answer(self, user_answer):
+        """Checks if the user's answer is correct."""
+        correct_answer = self.current_question.answer # Gets the correct answer from the current question
+        if user_answer.lower() == correct_answer.lower(): # Checks if the user's answer is the same as the correct answer
+            self.score += 1 # If it is, increments the score
+            print("You got it right!")
+        else:
+            print("That's wrong.")
+
+        print(f"Your current score is: {self.score}/{self.question_number}")
+        print("\n")
